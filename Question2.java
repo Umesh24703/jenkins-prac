b383c0191db87ffeb5febfef1294be9f3c4cee43
Umesh24703 <uy24703@gmail.com>
2025-02-18 19:25:18 +0530

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,5 +1,5 @@
 class Question2{
     public static void main(String args[]){
-      System.out.println("Question 2 working!!!!!!!")
+      System.out.println("Question 2 completed!!!!!!!")
     }
 }
